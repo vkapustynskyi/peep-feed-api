9d7fff2adab2a1e73d1fd2405e0563e6b84baad0
Volodymyr Kapustynskyi <volodymyr.kapustynskyi@kindgeek.com>
2023-04-25 23:38:27 +0300

    Initial commit

---
+++ src/main/java/com/vkapustynskyi/peepfeed/PeepFeedApplication.java
@@ -0,0 +1,13 @@
+package com.vkapustynskyi.peepfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeepFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeepFeedApplication.class, args);
+	}
+
+}
